8d68668c5481bc16af2b50bc14e9534fb74ee7c6
sadamcik <stadas@stadas.sk>
2016-01-29 14:09:57 +0100

    test

---
+++ app/src/main/java/sk/stadas/vavservices/VaVService.java
@@ -0,0 +1,53 @@
+package sk.stadas.vavservices;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class VaVService extends Service {
+    /** indicates how to behave if the service is killed */
+    int mStartMode;
+
+    /** interface for clients that bind */
+    IBinder mBinder;
+
+    /** indicates whether onRebind should be used */
+    boolean mAllowRebind;
+
+    public VaVService() {
+    }
+
+    /** The service is starting, due to a call to startService() */
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    /** A client is binding to the service with bindService() */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    /** Called when all clients have unbound with unbindService() */
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return mAllowRebind;
+    }
+
+    /** Called when a client is binding to the service with bindService()*/
+    @Override
+    public void onRebind(Intent intent) {
+
+    }
+
+    /** Called when The service is no longer used and is being destroyed */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+}
